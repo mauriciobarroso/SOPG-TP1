fedddb3c99cc22f72ca6275bb3430438b6d1e89f
Mauricio Barroso <mauriciobarroso1990@gmail.com>
2020-04-05 12:25:04 -0300

    se agregan archivos

---
+++ reader.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdint.h>
+
+#define FIFO_NAME       "myfifo"
+#define BUFFER_SIZE     300
+#define MESSAGES_LIMIT  10
+
+int main(void)
+{
+	uint8_t inputBuffer[ BUFFER_SIZE ];
+	uint32_t bytesRead;
+    int32_t returnCode, fd;
+    FILE *fpLOG, *fpSIGNALS ;
+    uint8_t messageCounter = 0;
+    /* Create named fifo. -1 means already exists so no action if already exists */
+    if ( ( returnCode = mknod(FIFO_NAME, S_IFIFO | 0666, 0 ) ) < -1  )
+    {
+        printf( "Error creating named fifo: %d\n", returnCode );
+        exit( 1 );
+    }
+    
+    /* Open named fifo. Blocks until other process opens it */
+	printf( "waiting for writers...\n" );
+	if ( ( fd = open( FIFO_NAME, O_RDONLY ) ) < 0 )
+    {
+        printf( "Error opening named fifo file: %d\n", fd );
+        exit( 1 );
+    }
+    
+    /* open syscalls returned without error -> other process attached to named fifo */
+	printf( "got a writer\n" );
+    /**/
+    fpLOG = fopen ( "log.txt", "a" );
+    if (fpLOG == NULL) 
+    { 
+        printf("Could not open Log.txt" ); 
+        return 0; 
+    } 
+
+    fpSIGNALS = fopen ("signals.txt", "a" );
+    if ( fpSIGNALS == NULL ) 
+    { 
+        printf( "Could not open Sign.txt" ); 
+        return 0; 
+    } 
+    /* Loop until read syscall returns a value <= 0 */
+	do
+	{
+        /* read data into local buffer */
+		if ( ( bytesRead = read( fd, inputBuffer, BUFFER_SIZE ) ) == -1)
+            perror( "read" );
+        else
+		{
+			inputBuffer[ bytesRead ] = '\0';
+			printf( "reader: read %d bytes: \"%s\"\n", bytesRead, inputBuffer );
+		}
+        /**/
+        if( !strncmp( "DATA:", inputBuffer, sizeof( 5 ) ) )
+        {           
+            fprintf( fpLOG, "%s\n", inputBuffer );
+            printf( "reader: \"%s\" saved in log.txt\n", inputBuffer );
+        }
+        else
+        {
+            fprintf( fpSIGNALS, "%s\n", inputBuffer );
+            printf( "reader: \"%s\" saved in signals.txt\n", inputBuffer );       
+        }
+
+        messageCounter++;
+	}
+ 
+	while ( bytesRead > 0 && messageCounter < MESSAGES_LIMIT );
+
+    fclose( fpLOG ); 
+    fclose( fpSIGNALS );
+
+	return 0;
+}
---
+++ writer.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <signal.h>
+
+#define FIFO_NAME "myfifo"
+#define BUFFER_SIZE 300
+
+volatile sig_atomic_t signalNumber;
+
+void handler1( int sig );
+void handler2( int sig );
+
+int main()
+{   
+    /**/
+    char outputBuffer[BUFFER_SIZE];
+	uint32_t bytesWrote;
+	int32_t returnCode, fd;
+    struct sigaction sa;
+
+    /**/
+    sa.sa_handler = handler1;
+    sa.sa_flags = 0;
+    if( sigemptyset( &sa.sa_mask ) == -1)
+    {
+        perror( "sigemptyset" );
+        exit( 1 );
+    }
+    if( sigaction( SIGUSR1, &sa, NULL ) == -1 )
+    {
+        perror( "sigaction" );
+        exit( 1 );
+    }
+
+    /**/
+    sa.sa_handler = handler2;
+    sa.sa_flags = 0;
+    if( sigemptyset( &sa.sa_mask ) == -1)
+    {
+        perror( "sigemptyset" );
+        exit( 1 );
+    }
+    if( sigaction( SIGINT, &sa, NULL ) == -1 )
+    {
+        perror( "sigaction" );
+        exit( 1 );
+    }
+
+    /* Create named fifo. -1 means already exists so no action if already exists */
+    if ( ( returnCode = mknod( FIFO_NAME, S_IFIFO | 0666, 0 ) ) < -1 )
+    {
+        printf( "Error creating named fifo: %d\n", returnCode );
+        exit( 1 );
+    }   
+
+    /* Open named fifo. Blocks until other process opens it */
+	printf( "waiting for readers...\n" );
+	if ( ( fd = open( FIFO_NAME, O_WRONLY ) ) < 0 )
+    {
+        printf( "Error opening named fifo file: %d\n", fd );
+        exit( 1 );
+    }
+
+    /* open syscalls returned without error -> other process attached to named fifo */
+	printf( "got a reader--type some stuff\n" );
+
+    /* Loop forever */
+	for( ;; )
+	{
+        /* Get some text from console */
+		fgets( outputBuffer, BUFFER_SIZE, stdin );
+
+        /**/   
+        char auxBuffer[ BUFFER_SIZE + sizeof( "DATA:" ) ];
+        if( signalNumber != 0 )
+        {
+            sprintf( auxBuffer, "SIGN:%d\n", signalNumber);
+            signalNumber = 0;
+        }
+        else   
+            sprintf( auxBuffer, "DATA:%s", outputBuffer );
+        
+        /* Write buffer to named fifo. Strlen - 1 to avoid sending \n char */
+		if (( bytesWrote = write( fd, auxBuffer, strlen( auxBuffer ) - 1 ) ) == -1 )
+            perror( "write" );
+        else
+            printf( "writer: wrote %d bytes\n", bytesWrote );
+	}
+	return 0;
+}
+
+void handler1( int sig )
+{
+    signalNumber = 1;
+}
+
+void handler2( int sig )
+{
+    signalNumber = 2;
+}
